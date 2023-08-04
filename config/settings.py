e0aa3deaaca03532d6c433b8596ca9319c6f0259
MaryamKalantarii <137641133+MaryamKalantarii@users.noreply.github.com>
2023-08-05 01:43:09 +0430

    S

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATIC_ROOT = BASE_DIR/'/static'
 MEDIA_ROOT = BASE_DIR/'/media'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 
